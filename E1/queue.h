5a00c50bb486c8b73b9f1041a7944d9c0dc64a4d
Ly Sandro <46694835+Ly54ndr0@users.noreply.github.com>
2019-03-19 18:13:44 -0300

    Exercise 1 completed

---
+++ E1/queue.h
@@ -0,0 +1,46 @@
+/* Solution for Exercise 1 from chapter 19 from */
+/* C programming, a modern approach by K N King */
+
+// THIS IS AN  INTERFACE FOR A QUEUE MODULE IN THE
+// FORM OF A HEADER FILE.
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+typedef int Item;
+
+/*
+queue_insert:
+Inserts an item at the end of a pre-set queue.
+*/
+bool queue_insert(Item item);
+
+/*
+queue_remove:
+Removes an  item from  he beginning of the pre-set
+queue.
+*/
+bool queue_remove(void);
+
+/*
+queue_return_first:
+returns the first item from the pre-set queue.
+*/
+Item queue_return_first(void);
+
+/*
+queue_return_last:
+returns the last item from the pre-set queue.
+*/
+Item queue_return_last(void);
+
+/*
+queue_is_empty:
+Testes   whether  the   pre-set  queue  is  empty,
+returning true or false.
+*/
+bool queue_is_empty(void);
+
+#endif
